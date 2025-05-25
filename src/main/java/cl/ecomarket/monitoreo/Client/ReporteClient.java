d6fdea985dc2117121005b6353bbf723181bd696
Renato <ren.ortegar@duocuc.cl>
2025-05-25 00:12:45 -0400

    Prueba 2 DTO

--- src/main/java/cl/ecomarket/monitoreo/Client/ReporteClient.java
+++ src/main/java/cl/ecomarket/monitoreo/Client/ReporteClient.java
@@ -1,13 +1,13 @@
 package cl.ecomarket.monitoreo.Client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import cl.ecomarket.monitoreo.DTO.MonitoreoDTO;
 
 @FeignClient(name = "user", url = "${https://smartsync-usuario-back-pruebas.onrender.com}")
 public interface ReporteClient {
-    @GetMapping("/api/v1/ecomarket/user/monitoreo")
+    @PostMapping("/enviarMonitoreo")
     void enviarMonitoreo(@RequestBody MonitoreoDTO monitoreoDTO);
 }
