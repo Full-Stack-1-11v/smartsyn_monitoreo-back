e2e51a487d96a7ff8958ce7d47774ab7ac5a089c
Renato <ren.ortegar@duocuc.cl>
2025-05-25 00:48:36 -0400

    Prueba 4 DTO

--- src/main/java/cl/ecomarket/monitoreo/Client/ReporteClient.java
+++ src/main/java/cl/ecomarket/monitoreo/Client/ReporteClient.java
@@ -8,6 +8,6 @@ import cl.ecomarket.monitoreo.DTO.MonitoreoDTO;
 
 @FeignClient(name = "user", url = "${url.usuario}")
 public interface ReporteClient {
-    @PostMapping("api/v1/user/enviarMonitoreo")
+    @PostMapping("/api/v1/monitoreo/enviarMonitoreo")
     void enviarMonitoreo(@RequestBody MonitoreoDTO monitoreoDTO);
 }
