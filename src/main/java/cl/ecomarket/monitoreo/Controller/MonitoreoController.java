15a94094affb03a39408acb5a959abbc7ee0099d
Renato <ren.ortegar@duocuc.cl>
2025-05-27 15:55:15 -0400

    Cambiar ruta URL

--- src/main/java/cl/ecomarket/monitoreo/Controller/MonitoreoController.java
+++ src/main/java/cl/ecomarket/monitoreo/Controller/MonitoreoController.java
@@ -107,7 +107,7 @@ public class MonitoreoController {
         }
     }
 
-    @GetMapping
+    @GetMapping("/obtenerReporte")
     public ResponseEntity<List<reporteDTO>> listarReporte() {
         List<reporteDTO> reportes = monitoreoService.obtenerReporte();
         if (reportes.isEmpty()) {
